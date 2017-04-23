85648535d6816cdfce86a69eb4c57cd114f81e92
Nick Cruz <ncruz@umich.edu>
2017-04-23 14:20:11 -0500

    Added a normal layout and customized the animation.

--- app/src/main/kotlin/me/nickcruz/propagation/MainActivity.kt
+++ app/src/main/kotlin/me/nickcruz/propagation/MainActivity.kt
@@ -2,6 +2,10 @@ package me.nickcruz.propagation
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.transition.Fade
+import android.transition.Slide
+import android.transition.TransitionSet
+import android.view.Gravity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.title = "Flight Details"
+
         swipeRefreshLayout.setOnRefreshListener {
-            FadeTransition(constraintLayout).start()
+            CustomTransition(sceneRoot = constraintLayout,
+                    startingView = imageIcon,
+                    transition = TransitionSet()
+                            .addTransition(Fade(Fade.IN).setStartDelay(400))
+                            .addTransition(Slide(Gravity.TOP)))
+                    .start()
             swipeRefreshLayout.isRefreshing = false
         }
     }
